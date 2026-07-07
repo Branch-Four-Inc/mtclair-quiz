2259864f39af82a828db5bdd14c939d3fc44257d
Swati Sharma <this.is.swati.sharma@gmail.com>
2026-07-06 21:26:50 -0400

    Changed next.config to be ESM compatible

--- .open-next/.build/durable-objects/queue.js
+++ .open-next/.build/durable-objects/queue.js
@@ -156,7 +156,7 @@ var DOQueueHandler = class extends DurableObject {
         method: "HEAD",
         headers: {
           // This is defined during build
-          "x-prerender-revalidate": "7c74c0fbd4e35a73e06538a8dbebd068",
+          "x-prerender-revalidate": "a994a992be48a18494e0c34ab2584f33",
           "x-isr": "1"
         },
         // This one is kind of problematic, it will always show the wall time of the revalidation to `this.revalidationTimeout`
@@ -180,7 +180,7 @@ var DOQueueHandler = class extends DurableObject {
           "INSERT OR REPLACE INTO sync (id, lastSuccess, buildId) VALUES (?, unixepoch(), ?)",
           // We cannot use the deduplication id because it's not unique per route - every time a route is revalidated, the deduplication id is different.
           `${host}${url}`,
-          "UcNnpXmt8TxPSt6xHPnB3"
+          "HGL3ztOEnn7o-OybBvl_N"
         );
       }
       this.routeInFailedState.delete(msg.MessageDeduplicationId);
@@ -232,7 +232,7 @@ var DOQueueHandler = class extends DurableObject {
     }
     this.routeInFailedState.set(msg.MessageDeduplicationId, updatedFailedState);
     if (!this.disableSQLite) {
-      this.sql.exec("INSERT OR REPLACE INTO failed_state (id, data, buildId) VALUES (?, ?, ?)", msg.MessageDeduplicationId, JSON.stringify(updatedFailedState), "UcNnpXmt8TxPSt6xHPnB3");
+      this.sql.exec("INSERT OR REPLACE INTO failed_state (id, data, buildId) VALUES (?, ?, ?)", msg.MessageDeduplicationId, JSON.stringify(updatedFailedState), "HGL3ztOEnn7o-OybBvl_N");
     }
     await this.addAlarm();
   }
@@ -256,8 +256,8 @@ var DOQueueHandler = class extends DurableObject {
       return;
     this.sql.exec("CREATE TABLE IF NOT EXISTS failed_state (id TEXT PRIMARY KEY, data TEXT, buildId TEXT)");
     this.sql.exec("CREATE TABLE IF NOT EXISTS sync (id TEXT PRIMARY KEY, lastSuccess INTEGER, buildId TEXT)");
-    this.sql.exec("DELETE FROM failed_state WHERE buildId != ?", "UcNnpXmt8TxPSt6xHPnB3");
-    this.sql.exec("DELETE FROM sync WHERE buildId != ?", "UcNnpXmt8TxPSt6xHPnB3");
+    this.sql.exec("DELETE FROM failed_state WHERE buildId != ?", "HGL3ztOEnn7o-OybBvl_N");
+    this.sql.exec("DELETE FROM sync WHERE buildId != ?", "HGL3ztOEnn7o-OybBvl_N");
     const failedStateCursor = this.sql.exec("SELECT * FROM failed_state");
     for (const row of failedStateCursor) {
       this.routeInFailedState.set(row.id, JSON.parse(row.data));
--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,18 @@
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
+
+initOpenNextCloudflareForDev();
+
+/** @type {import("next").NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
+
+export default nextConfig;
+
+/*
+// code for standard .js file, commented out because switched to .mjs file for ESM
 const { initOpenNextCloudflareForDev } = require("@opennextjs/cloudflare");
 
 initOpenNextCloudflareForDev();
@@ -5,7 +20,7 @@ initOpenNextCloudflareForDev();
 module.exports = {
   typescript: { ignoreBuildErrors: true },
   // eslint: { ignoreDuringBuilds: true }
-};
+}; */
 
 /*
 /** @type {import('next').NextConfig} * /
